b30ae6fdf0c49feca63454d4d4faf58cf863843a
noah-solomon <75463135+noah-solomon@users.noreply.github.com>
2021-03-24 17:51:49 -0600

    Add basic person demographics and add GET and POST for /api/me/ (#18)
    * Add DS_STORE to .gitignore
    
    * Add User model and GET & POST for /api/me/

--- src/person/models.py
+++ src/person/models.py
@@ -1,9 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
-# Create your models here.
 class Person(models.Model):
     id_token = models.TextField(null=True)
     net_id = models.CharField(max_length=10)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, unique=True, default=None
+    )
+    hometown = models.CharField(max_length=50, default=None, null=True)
+    profile_pic_url = models.CharField(max_length=100, default=None, null=True)
+    facebook_url = models.CharField(max_length=50, default=None, null=True)
+    instagram_username = models.CharField(max_length=30, default=None, null=True)
+    graduation_year = models.CharField(max_length=4, default=None, null=True)
+    pronouns = models.CharField(max_length=20, default=None, null=True)
--- src/person/serializers.py
+++ src/person/serializers.py
@@ -1,6 +1,6 @@
-
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
 
 class AuthenticateSerializer(serializers.ModelSerializer):
     access_token = serializers.SerializerMethodField(method_name="get_access_token")
@@ -17,3 +17,29 @@ class AuthenticateSerializer(serializers.ModelSerializer):
 
     def get_access_token(self, instance):
         return self.context.get("access_token")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    net_id = serializers.CharField(source="person.net_id")
+    hometown = serializers.CharField(source="person.hometown")
+    profile_pic_url = serializers.CharField(source="person.profile_pic_url")
+    facebook_url = serializers.CharField(source="person.facebook_url")
+    instagram_username = serializers.CharField(source="person.instagram_username")
+    graduation_year = serializers.CharField(source="person.graduation_year")
+    pronouns = serializers.CharField(source="person.pronouns")
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "net_id",
+            "first_name",
+            "last_name",
+            "hometown",
+            "profile_pic_url",
+            "facebook_url",
+            "instagram_username",
+            "graduation_year",
+            "pronouns",
+        )
+        read_only_fields = fields
