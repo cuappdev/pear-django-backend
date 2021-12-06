448672f09c5e28247a27bf3e9258d3065433975e
Gonzalo Gonzalez <gg387@cornell.edu>
2021-12-06 11:40:54 -0500

    Matching cronjob [DEV] (#83)
    * Create cronjob using celery beats
    
    * Add submodule for Pear algorithm
    
    * Add cronjob stub
    
    * Finish matching cronjob

--- Dockerfile
+++ Dockerfile
@@ -10,10 +10,12 @@ COPY /src/ .
 
 COPY ./wait-for-it.sh wait-for-it.sh
 COPY ./requirements.txt requirements.txt
+COPY ./pear-algorithm/requirements.txt algorithm_reqs.txt
 
 RUN ["chmod", "+x", "/usr/src/app/wait-for-it.sh"]
 
 RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r algorithm_reqs.txt
 
 EXPOSE 8000
 
---
+++ src/match/tasks.py
@@ -0,0 +1,75 @@
+import datetime
+import logging
+import os
+import sys
+
+from celery import Celery
+from celery.signals import after_setup_logger
+from django.utils import timezone
+from django_celery_beat.models import IntervalSchedule
+from django_celery_beat.models import PeriodicTask
+from match import match_status
+from match.controllers.create_match_controller import CreateMatchController
+from match.models import Match
+from person.models import Person
+
+# Get Pear algorithm
+current_dir = os.path.dirname(os.path.abspath(__file__))
+submodule_path = f"{current_dir}/../../pear-algorithm/src"
+sys.path.insert(0, submodule_path)
+from main import main
+
+app = Celery()
+
+# https://www.distributedpython.com/2018/08/28/celery-logging/
+logger = logging.getLogger(__name__)
+
+
+@after_setup_logger.connect
+def setup_loggers(logger, *args, **kwargs):
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+
+@app.task
+def matcher():
+    # Cancel previous matches that were not completed. TODO verify this is what we want to do
+    today = timezone.now()
+    last_week = today - datetime.timedelta(
+        days=7
+    )  # should be before pears are released but after matching
+    previous_matches = Match.objects.filter(created_date__range=[last_week, today])
+    print(f"Previous: {previous_matches}")
+    unfinished_matches = previous_matches.exclude(
+        status__in=[match_status.CANCELED, match_status.INACTIVE]
+    )
+    print(f"Unfinished: {unfinished_matches}")
+    unfinished_matches.update(status=match_status.CANCELED)
+
+    # Generate pears
+    users = Person.objects.all()
+    pears = main(users, logger)
+    match_creator = CreateMatchController(None, None, return_status=True)
+
+    # Create new matches
+    for pear in pears:
+        match_creator._data = {"ids": pear}
+        success, error_msg = match_creator.process()
+        if not success:
+            print(f"Match error between {pear}: {error_msg}")
+
+
+schedule, _ = IntervalSchedule.objects.get_or_create(
+    every=1,
+    period=IntervalSchedule.MINUTES,
+)
+
+PeriodicTask.objects.get_or_create(
+    interval=schedule,
+    name="Pear Algorithm",
+    task="match.tasks.matcher",
+)
