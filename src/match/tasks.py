2d74001bc837660478658bde4cbd771a7a5abb80
Noah Solomon <njs99@cornell.edu>
2022-03-14 22:55:42 -0400

    Updated simple task as proof of concept

--- src/match/tasks.py
+++ src/match/tasks.py
@@ -1,3 +1,5 @@
+import datetime
+
 from celery import shared_task
 from django.contrib.auth.models import User
 from django.db.models import Q
@@ -8,19 +10,37 @@ from match.controllers.create_match_controller import CreateMatchController
 from match.models import Match
 from pear_algorithm.src.main import main as pear_algorithm
 
+# PROOF OF CONCEPT
+
 schedule, _ = IntervalSchedule.objects.get_or_create(
     every=1,
     period=IntervalSchedule.MINUTES,
 )
 
+
+@shared_task
+def update_noah():
+    noah = User.objects.get(person__net_id="njs99")
+    noah.user.last_name = "Solomon " + datetime.datetime.now().strftime(
+        "%m/%d/%Y, %H:%M:%S"
+    )
+    noah.save()
+    noah.user.save()
+    return "Saved Noah"
+
+
+PeriodicTask.objects.get_or_create(
+    # TODO: Change to crontab schedule after testing on
+    # crontab=schedule,
+    interval=schedule,
+    name="Update Noah",
+    task="match.tasks.update_noah",
+)
+
+# END PROOF OF CONCEPT
+
 # TODO: Test above schedule on dev w/ more users to see if this works,
-#  eventually change to
-# from django_celery_beat.models import CrontabSchedule
-# schedule, _ = CrontabSchedule.objects.get_or_create(
-#     minute='0',
-#     hour='12',
-#     day_of_week='7',
-# )
+#  eventually change to task below
 
 
 @shared_task
@@ -46,8 +66,9 @@ def matcher():
             print(f"Match error between {pear}: {error_msg}")
 
 
-PeriodicTask.objects.get_or_create(
-    crontab=schedule,
-    name="Pear Algorithm",
-    task="match.tasks.matcher",
-)
+# TODO: uncomment after proof of concept works on dev server
+# PeriodicTask.objects.get_or_create(
+#     crontab=schedule,
+#     name="Matching Algorithm",
+#     task="match.tasks.matcher",
+# )
