0f7e1637fac5002100b08226b35764b77e608e8d
Noah Solomon <75463135+noah-solomon@users.noreply.github.com>
2022-03-21 01:32:29 -0400

    Allow users to block and unblock each other (#98)
    * Add endpoints for blocking and unblocking
    
    * Hide blocked users when seeing my matches
    
    * Add hiding of blocked users if they are the current match
    
    * Added 403 response for blocking yourself
    
    * Add 403 response for unblocking yourself
    
    * exluding -> excluding

--- src/api/urls.py
+++ src/api/urls.py
@@ -16,11 +16,12 @@ from match.views import CurrentMatchView
 from match.views import MatchView
 from match.views import MultipleMatchesView
 from match.views import MyMatchesView
-from match.views import UserMatchesView
 from person.views import AuthenticateView
+from person.views import BlockUserView
 from person.views import MassMessageView
 from person.views import MeView
 from person.views import SendMessageView
+from person.views import UnblockUserView
 from person.views import UsersView
 from person.views import UserView
 from prompt.views import PromptsView
@@ -38,7 +39,9 @@ urlpatterns = [
     path("me/", MeView.as_view(), name="me"),
     path("users/", UsersView.as_view(), name="users"),
     path("users/<int:id>/", UserView.as_view(), name="user"),
-    path("users/<int:id>/matches/", UserMatchesView.as_view(), name="user_matches"),
+    path("users/<int:id>/matches/", AllMatchesView.as_view(), name="user_matches"),
+    path("users/<int:id>/block/", BlockUserView.as_view(), name="block_user"),
+    path("users/<int:id>/unblock/", UnblockUserView.as_view(), name="unblock_user"),
     # Push Notification URLs
     path("users/<int:id>/message/", SendMessageView.as_view(), name="user_messages"),
     path("mass-message/", MassMessageView.as_view(), name="user_messages"),
