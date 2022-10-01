52a042e994fd5893cd44ccbceb17dad3cf867c02
Jessica Sylvester <jzs27@cornell.edu>
2022-10-01 15:05:14 -0400

    Formatting issues

--- src/person/tests/test_person.py
+++ src/person/tests/test_person.py
@@ -30,6 +30,12 @@ class UserProfileTests(PearTestCase):
     def test_get_users(self):
         response = self.client.get(self.USERS_URL, format="json")
         content = json.loads(response.content)
-
         self.assertTrue(content.get("success"))
         self.assertEqual(response.status_code, 200)
+
+    def test_update_user(self):
+        response = self.client.get(self.ME_URL, format="json")
+        request_data = {"family_name": "Pumariega"}
+        response = self.client.post(self.ME_URL, request_data, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Gonzalez")
